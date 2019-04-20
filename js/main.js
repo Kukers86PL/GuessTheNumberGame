9b1324501a09b0a92acd3a19254f8b5c3ee40120
Łukasz Grzelak <lukasz.grzelak.86@gmail.com>
2019-04-20 08:36:34 +0200

    Nickname generation added

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 window.onload = () => {
 	'use strict';
+	getClientData();
 
 	if ("serviceWorker" in navigator) {
 		if (!navigator.serviceWorker.controller) {
@@ -10,4 +11,55 @@ window.onload = () => {
 			})
 		}
 	}
+}
+
+function getClientData() {
+	var request = self.indexedDB.open('GuessTheNumberGame', 1);
+
+	request.onsuccess = function (event) {
+
+		// get database from event
+		var db = event.target.result;
+
+		// create transaction from database
+		var transaction = db.transaction('client_data', 'readwrite');
+
+		// get store from transaction
+		var client_data_store = transaction.objectStore('client_data');
+
+		client_data_store.count().onsuccess = function (event) {
+			if (event.target.result == 0) {
+				var client_id = getRandom();
+				var client_nickname = 'quest_' + client_id;
+				var client_data = {
+					id: 1,
+					client: client_id,
+					nickname: client_nickname
+				};
+				client_data_store.add(client_data);
+				document.getElementById('nickname').value = client_nickname;
+			} else {
+				client_data_store.get(1).onsuccess = function (event) {
+					document.getElementById('nickname').value = event.target.result.nickname;
+				};
+			}
+		};
+	};
+
+	request.onerror = function (event) {
+		console.log('[onerror]', request.error);
+	};
+
+	request.onupgradeneeded = function (event) {
+		var db = event.target.result;
+		var client_data_store = db.createObjectStore('client_data', {
+				keyPath: 'id'
+			});
+	};
+}
+
+function getRandom() {
+	var min = Math.ceil(0);
+	var max = Math.floor(1000000);
+	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
